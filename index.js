6bbb4c8b0eb9d671092b584f1a843b11827ca200
Jubair <jubair.ahmed2838@gmail.com>
2023-08-14 21:46:31 +0600

    login page finished

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-ECHO is on.
+document.getElementById('login-btn').addEventListener('click', () => {
+    // Define UI variables
+    const email = document.getElementById('input-email').value;
+    const password = document.getElementById('input-password').value;
+    
+    // verify user
+    if(email === ''  || password === '') {
+        alert('Please enter valid input')
+    }
+    else if(email === 'joeyloves@food.com' && password === 'iloverachel') {
+        location.href = 'bank.html'
+    } else {
+       alert('Chandler, stop trying to login');
+    }
+})
